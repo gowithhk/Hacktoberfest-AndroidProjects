746561ba74fd7a0d8163e9cfceb38af178fd4977
harshkumar-hacktoberfest <72377904+harshkumar-hacktoberfest@users.noreply.github.com>
2020-10-05 14:41:44 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/harshcalculator/MainActivity.java
+++ app/src/main/java/com/example/harshcalculator/MainActivity.java
@@ -23,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
- 	button0 = (Button) findViewById(R.id.button0);
+ 	
+        button0 = (Button) findViewById(R.id.button0);
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
         button3 = (Button) findViewById(R.id.button3);
@@ -34,14 +35,166 @@ public class MainActivity extends AppCompatActivity {
         button8 = (Button) findViewById(R.id.button8);
         button9 = (Button) findViewById(R.id.button9);
         button10 = (Button) findViewById(R.id.button10);
-        buttonAdd = (Button) findViewById(add);
-        buttonSub = (Button) findViewById(sub);
-        buttonMul = (Button) findViewById(mul);
-        buttonDivision = (Button) findViewById(div);
-        buttonC = (Button) findViewById(C);
-        buttonEqual = (Button) findViewById(eql);
+        buttonAdd = (Button) findViewById(R.id.buttonadd);
+        buttonSub = (Button) findViewById(R.id.buttonsub);
+        buttonMul = (Button) findViewById(R.id.buttonmul);
+        buttonDivision = (Button) findViewById(R.id.buttondiv);
+        buttonC = (Button) findViewById(R.id.buttonC);
+        buttonEqual = (Button) findViewById(R.id.buttoneql);
+        harshEditText = (EditText) findViewById(R.id.edt1);
 
 
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                harshEditText.setText(harshEditText.getText() + "1");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                harshEditText.setText(harshEditText.getText() + "2");
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                harshEditText.setText(harshEditText.getText() + "3");
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                harshEditText.setText(harshEditText.getText() + "4");
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                harshEditText.setText(harshEditText.getText() + "5");
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                harshEditText.setText(harshEditText.getText() + "6");
+            }
+        });
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                harshEditText.setText(harshEditText.getText() + "7");
+            }
+        });
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                harshEditText.setText(harshEditText.getText() + "8");
+            }
+        });
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                harshEditText.setText(harshEditText.getText() + "9");
+            }
+        });
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                harshEditText.setText(harshEditText.getText() + "0");
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (harshEditText == null) {
+                    harshEditText.setText("");
+                } else {
+                    mValueOne = Float.parseFloat(harshEditText.getText() + "");
+                    harshAddition = true;
+                    harshEditText.setText(null);
+                }
+            }
+        });
+
+        buttonSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(harshEditText.getText() + "");
+                mSubtract = true;
+                harshEditText.setText(null);
+            }
+        });
+
+        buttonMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(harshEditText.getText() + "");
+                harshMultiplication = true;
+                harshEditText.setText(null);
+            }
+        });
+
+        buttonDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(harshEditText.getText() + "");
+                harshDivision = true;
+                harshEditText.setText(null);
+            }
+        });
+
+        buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueTwo = Float.parseFloat(harshEditText.getText() + "");
+
+                if (harshAddition == true) {
+                    harshEditText.setText(mValueOne + mValueTwo + "");
+                    harshAddition = false;
+                }
+
+                if (mSubtract == true) {
+                    harshEditText.setText(mValueOne - mValueTwo + "");
+                    mSubtract = false;
+                }
+
+                if (harshMultiplication == true) {
+                    harshEditText.setText(mValueOne * mValueTwo + "");
+                    harshMultiplication = false;
+                }
+
+                if (harshDivision == true) {
+                    harshEditText.setText(mValueOne / mValueTwo + "");
+                    harshDivision = false;
+                }
+            }
+        });
+
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                harshEditText.setText("");
+            }
+        });
+
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                harshEditText.setText(harshEditText.getText() + ".");
+            }
+        });
 
     }
 }
